398b48603a0ecfc9420ebefb570b56170f4c80ad
Jordan C <jordan.c4922@gmail.com>
2024-07-19 14:43:26 -0600

    Add website and appstore links to experience section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -6,7 +6,9 @@ const defaultExperience = [
     title: 'iOS Developer',
     company: 'Wander App Inc',
     duration: 'February 2021 - October 2023',
-    description: 'Worked on developing and maintaining the company\'s iOS application using Swift.\nWhat I did:\n- Transition the UIKit-based app to SwiftUI\n- Majorly improved the performance of the Mapbox map\n- Added the ability to save map tiles for offline use\n- Transitioned the app from GCD completion handler-based concurrency to async-await\n- General maintanence and bug fixing',
+    description: 'Worked on developing and maintaining the company\'s iOS application using Swift.\nWhat I did:\n- Transition the UIKit-based app to SwiftUI\n- Majorly improved the performance of the Mapbox map\n- Added the ability to save map tiles for offline use\n- Transitioned the app from GCD completion handler-based concurrency to async-await\n- General maintanence and bug fixing\n\nPlease note, the app now uses React Native and no longer reflects the work that I did while there.',
+    website: "https://wandermaps.com/",
+    appstore: "https://apps.apple.com/us/app/wander-maps-navigation/id1556697512"
   },
   {
     id: 2,
@@ -33,9 +35,9 @@ export default function Experience() {
             <h3>{exp.title}</h3>
             <p>{exp.company ?? ''}</p>
             <p>{exp.duration}</p>
-            {
-              descriptionParagraphs.map(({key, text}) => <p key={key}>{text}</p>)
-            }
+            { descriptionParagraphs.map(({key, text}) => <p key={key}>{text}</p>) }
+            { exp.website && <a style={{ marginRight: "25px" }} href={exp.website}>Website</a> }
+            { exp.appstore && <a href={exp.appstore}>App Store</a> }
           </div>
         )
       })}
